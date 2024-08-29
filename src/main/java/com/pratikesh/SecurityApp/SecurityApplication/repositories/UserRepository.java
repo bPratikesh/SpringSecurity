aa6538b40ee5eed433bbd93553279bff45b20058
Pratikesh <pratikeshb9@gmail.com>
2024-08-29 21:05:22 +0530

    Implemented core components of Spring security

---
+++ src/main/java/com/pratikesh/SecurityApp/SecurityApplication/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.pratikesh.SecurityApp.SecurityApplication.repositories;
+
+import com.pratikesh.SecurityApp.SecurityApplication.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByEmail(String email);
+}
